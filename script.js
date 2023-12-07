05e15c59b1b78ee63674e1734238f9c1a766900d
Nick Savchenko <NickSvko@gmail.com>
2023-12-07 18:23:45 +0200

    Expand html structure

--- script.js
+++ script.js
@@ -3,24 +3,11 @@ const grid = document.querySelector('.grid');
 createGrid();
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 function createGrid() {
     let line;
     for(let i = 1; i <= GRID_SIZE; i++) {
         line = document.createElement('div');
-        line.setAttribute('class', 'l' + i)
+        line.setAttribute('class', `line l${i}`)
         createColumns(line);
         grid.appendChild(line);
     }
@@ -30,7 +17,8 @@ function createColumns(line) {
     let column;
     for(i = 1; i <= GRID_SIZE; i++) {
         column = document.createElement('div');
-        column.setAttribute('class','c' + i);
+        column.setAttribute('class',``);
+        column.setAttribute('class',  `column c${i}`)
         line.appendChild(column);
     }
 }
