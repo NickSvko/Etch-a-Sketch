7c6c66350ecaf35d0d5818f211ce49848e8131c8
Nick Savchenko <NickSvko@gmail.com>
2023-12-08 12:59:32 +0200

    Add a function to draw on the board

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 let gridSize = 16;
 const grid = document.querySelector('.grid');
-const drawButton = querySelector('#drawBtn');
-const eraseButton = querySelector('#eraseBtn');
-const clearButton = querySelector('#ClearBtn');
-const colorPicker = querySelector('#colorPicker');
-const sizeSlider = querySelector('#sizeSlider');
-const sizeDisplay = querySelector('#sizeDisplay');
+const drawButton = document.querySelector('#drawBtn');
+const eraseButton = document.querySelector('#eraseBtn');
+const clearButton = document.querySelector('#ClearBtn');
+const colorPicker = document.querySelector('#colorPicker');
+const sizeSlider = document.querySelector('#sizeSlider');
+const sizeDisplay = document.querySelector('#sizeDisplay');
+
+/* Defines whether the mouse is currently pressed to know whether to make changes to the board */
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
 
 createGrid(gridSize);
+drawButton.addEventListener('click', draw);
 
+
+function draw() {
+    let squares = document.querySelectorAll('.column');
+    squares.forEach(square => square.addEventListener('mouseover', () => colorSquare(square)));
+}
+
+function initializeEvents(square) {
+    square.addEventListener('mouseover', () => colorSquare(square));
+    square.addEventListener('onmouseup')
+}
+
+function colorSquare(square) {
+    if(mouseDown) {
+        square.style.backgroundColor = colorPicker.value;
+    }
+}
 
 function createGrid(size) {
     let line;
