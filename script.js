1a260bbb61350c30f8a7083502fe460e8a68437f
Nick Savchenko <NickSvko@gmail.com>
2023-12-08 13:33:48 +0200

    Add eraseFromBoard and ClearBoard functions

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ const PRESSED_BACKGROUND = 'rgb(110, 113, 115)';
 const RELEASE_BACKGROUND  = 'rgb(244, 240, 234)';
 const PRESSED_COLOR = 'white';
 const RELEASE_COLOR = 'black';
+const BOARD_COLOR = 'white';
 
 
 const grid = document.querySelector('.grid');
 const drawButton = document.querySelector('#drawBtn');
 const eraseButton = document.querySelector('#eraseBtn');
-const clearButton = document.querySelector('#ClearBtn');
+const clearButton = document.querySelector('#clearBtn');
 const colorPicker = document.querySelector('#colorPicker');
 const sizeSlider = document.querySelector('#sizeSlider');
 const sizeDisplay = document.querySelector('#sizeDisplay');
@@ -21,25 +22,38 @@ document.body.onmouseup = () => (mouseDown = false);
 
 createGrid(gridSize);
 const squares = document.querySelectorAll('.column');
-drawButton.addEventListener('click', draw);
-drawButton.addEventListener('click', draw);
+drawButton.addEventListener('click', drawOnBoard);
+eraseButton.addEventListener('click', eraseFromBoard);
+clearButton.addEventListener('click', clearBoard);
 
 
-function draw() {
-    press(drawButton);
-    release(eraseButton);
-    let squares = document.querySelectorAll('.column');
+function drawOnBoard() {
+    pressButton(drawButton);
+    releaseButtons(eraseButton);
     squares.forEach(square => square.addEventListener('mouseover', () => colorSquare(square)));
 }
 
-function press(button) {
+function eraseFromBoard() {
+    pressButton(eraseButton);
+    releaseButtons(drawButton);
+    squares.forEach(square => square.addEventListener('mouseover', () => eraseSquare(square)));
+}
+
+function clearBoard() {
+    releaseButtons(drawButton, eraseButton);
+    squares.forEach(square => square.style.backgroundColor = BOARD_COLOR);
+}
+
+function pressButton(button) {
     button.style.color = PRESSED_COLOR;
     button.style.backgroundColor = PRESSED_BACKGROUND;
 }
 
-function release(button) {
-    button.style.color = RELEASE_COLOR;
-    button.style.backgroundColor = RELEASE_BACKGROUND;
+function releaseButtons(...buttons) {
+    buttons.forEach(button => {
+        button.style.color = RELEASE_COLOR;
+        button.style.backgroundColor = RELEASE_BACKGROUND;
+    })
 }
 
 function initializeEvents(square) {
@@ -49,6 +63,13 @@ function initializeEvents(square) {
 function colorSquare(square) {
     if(mouseDown) {
         square.style.backgroundColor = colorPicker.value;
+    }
+}
+
+
+function eraseSquare(square) {
+    if(mouseDown) {
+        square.style.backgroundColor = BOARD_COLOR;
     }
 }
 
